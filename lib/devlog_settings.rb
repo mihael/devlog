1e3e08dcf632c3c9e55b8ebb74f947e4a3068403
mihael <kitschmaster@gmail.com>
2017-12-06 22:19:40 +0100

    First look at the settings issue, initial implementation of a Hash based settings object which loads from yaml file

--- lib/devlog_settings.rb
+++ lib/devlog_settings.rb
@@ -1,7 +1,31 @@
+require 'yaml'
 require 'ostruct'
+#require 'pry'
 
+#
 module Devlog
-  Settings = OpenStruct.new(
-    debug: false,
-  )
+  # Settings - keep it simple: lower_case: one_level_only
+  # Allow settings.key besides settings[:key]
+  # If the method name exists as a key within this Hash, fetch it.
+  class Settings < Hash
+    def method_missing(m, *args, &block)
+      if key?(m)
+        fetch m
+      elsif key?(m.to_s)
+        fetch m.to_s
+      else
+        super
+      end
+    end
+  end
+
+  def load_settings(file)
+    @settings = Settings[YAML.load_file(file)]
+  rescue
+    # Please add devlog.yml to configure.
+  end
+
+  def settings
+    @settings
+  end
 end
--- test/devlog_settings_test.rb
+++ test/devlog_settings_test.rb
@@ -2,7 +2,29 @@ require 'test_helper'
 
 # Test the settings
 class DevlogSettingsTest < Test::Unit::TestCase
-  def test_settings_default
-    assert(Devlog::Settings.debug == false, 'debug should be false')
+  def setup
+    load_settings(File.join(File.dirname(__FILE__),
+                        TEST_FILES_PATH, 'test_settings.yml'))
+  end
+
+  def test_settings
+    assert(settings.is_a?(Settings) == true, 'settings is a Hash')
+  end
+
+  def test_adding_setting
+    settings[:new_setting] = 'added'
+    assert(settings.new_setting == 'added', 'new_setting should be defined')
+  end
+
+  def test_changing_setting_on_the_fly
+    settings[:new_setting] = 'added'
+    assert(settings.new_setting == 'added', 'new_setting should be defined')
+    settings[:new_setting] = 'mod'
+    assert(settings.new_setting == 'mod', 'new_setting should be modified')
+  end
+
+  def test_loading_from_yaml
+    assert(settings.devlog_file == 'devlog.markdown',
+           'example_setting should be loaded')
   end
 end
