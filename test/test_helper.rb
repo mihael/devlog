5ac00113b4144651a5354703187a6ae89e4530a5
mihael <kitschmaster@gmail.com>
2019-09-20 10:35:01 +0200

    Refactor test helper slightly

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,6 +12,10 @@ end
 TEST_FILES_PATH = 'test_devlogs'.freeze
 TEMP_PATH = '../tmp'.freeze
 
+def parse_test_devlog(filename)
+  parse_devlog_now(File.join(File.dirname(__FILE__), TEST_FILES_PATH, filename))
+end
+
 def load_devlog
   @tajm = parse_devlog_now(File.join(File.dirname(__FILE__), '..', 'devlog.markdown'))
   puts "#{@tajm.coding_session_time} #{@tajm.com_session_time} #{@tajm.payed_time}"
@@ -29,7 +33,7 @@ def load_devlog_now
 end
 
 def load_devlog_test
-  @tajm_test = parse_devlog_now(File.join(File.dirname(__FILE__), TEST_FILES_PATH, 'test_devlog.markdown'))
+  @tajm_test = parse_test_devlog('test_devlog.markdown')
   puts "#{@tajm_test.coding_session_time} #{@tajm_test.com_session_time} #{@tajm_test.payed_time}"
   assert(@tajm_test.coding_session_time>0, "truth")
   assert(@tajm_test.com_session_time>0, "love")
@@ -37,13 +41,17 @@ def load_devlog_test
 end
 
 def load_devlog_stat
-  @tajm_stat = parse_devlog_now(File.join(File.dirname(__FILE__), TEST_FILES_PATH, 'test_stats_devlog.markdown'))
+  @tajm_stat = parse_test_devlog('test_stats_devlog.markdown')
 end
 
 def load_devlog_single
-  @tajm_single = parse_devlog_now(File.join(File.dirname(__FILE__), TEST_FILES_PATH, 'test_single_devlog.markdown'))
+  @tajm_single = parse_test_devlog('test_single_devlog.markdown')
 end
 
 def load_devlog_negative
-  @tajm_negative = parse_devlog_now(File.join(File.dirname(__FILE__), TEST_FILES_PATH, 'test_negative_devlog.markdown'))
+  @tajm_negative = parse_test_devlog('test_negative_devlog.markdown')
+end
+
+def load_devlog_weekly
+  @tajm_weekly = parse_test_devlog('test_weekly_devlog.markdown')
 end
